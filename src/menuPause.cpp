29ec22627edb89a2b203e206614b02d1a6bace28
hhplinh <huynhhaphuonglinh@gmail.com>
2023-11-03 12:56:58 +0700

    update

--- src/menuPause.cpp
+++ src/menuPause.cpp
@@ -1,22 +1,22 @@
-#include "menu.hpp"
+#include "menuPause.hpp"
 
 #include "maincharacter.hpp"
 #include "map.hpp"
 
-menu::menu(data *data)
+menuPause::menuPause(data *data)
     : _data(data), m_buttonsSelected(NUM_BUTTONS, false), m_buttonsPressed(NUM_BUTTONS, false)
 {
     m_buttonsSelected[0] = true;
 }
-menu::~menu() {}
+menuPause::~menuPause() {}
 
-void menu::init()
+void menuPause::init()
 {
-    _data->_assets->addTexture(BACKGROUND, "resources//Texture//bgMenu.png");
-    _data->_assets->addTexture(ROAD, "resources//Texture//River.png");
-    _data->_assets->addTexture(RIVER, "resources//Texture//road.png");
+    // _data->_assets->addTexture(BACKGROUND, "resources//Texture//bgMenu.png");
+    // _data->_assets->addTexture(ROAD, "resources//Texture//River.png");
+    // _data->_assets->addTexture(RIVER, "resources//Texture//road.png");
 
-    _data->_assets->addFont(MAIN_FONT, "resources//Font//LilitaOne-Regular.ttf");
+    // _data->_assets->addFont(MAIN_FONT, "resources//Font//LilitaOne-Regular.ttf");
 
     background.setTexture(&_data->_assets->getTexture(BACKGROUND));
     background.setSize(sf::Vector2f(1920, 1080));
@@ -36,7 +36,7 @@ void menu::init()
         m_buttons.push_back(button);
     }
 }
-void menu::processInput()
+void menuPause::processInput()
 {
     sf::Event event;
     while (_data->_window->pollEvent(event))
@@ -80,125 +80,114 @@ void menu::processInput()
     }
 }
 
-    void menu::update()
+void menuPause::update()
+{
+
+    setColorSelect(m_buttons, m_buttonsSelected);
+
+    if (m_buttonsPressed[RESUME])
     {
+        m_buttonsPressed[RESUME] = false;
+        _data->_states->removeState();
+    }
+    else if (m_buttonsPressed[MAIN_MENU])
+    {
+        m_buttonsPressed[MAIN_MENU] = false;
+        _data->_states->removeStateUntilOne();
+    }
+}
 
-        setColorSelect(m_buttons, m_buttonsSelected);
+void menuPause::draw()
+{
+    _data->_window->clear();
+    _data->_window->draw(background);
 
-        if (m_buttonsPressed[PLAY])
+    // condition for loading game, if there is no save file, the load game button will be disabled
+
+    for (int i = 0; i < m_buttons.size(); i++)
+    {
+        _data->_window->draw(m_buttons[i]);
+    }
+    _data->_window->display();
+}
+
+bool menuPause::isOnlyOneButtonOn(const std::vector<bool> &buttons)
+{
+    int count = 0;
+    for (int i = 0; i < buttons.size(); i++)
+    {
+        if (buttons[i] == true)
         {
-            m_buttonsPressed[PLAY] = false;
-            _data->_states->addState(new maincharacter(_data));
-        }
-        else if (m_buttonsPressed[EXIT])
-        {
-            m_buttonsPressed[EXIT] = false;
-            _data->_window->close();
-        }
-        else if (m_buttonsPressed[LOAD])
-        {
-            m_buttonsPressed[LOAD] = false;
-            // Implement your "Load Game" logic here
-            _data->_states->addState(new maincharacter(_data));
-        }
-        else if (m_buttonsPressed[SETTINGS])
-        {
-            m_buttonsPressed[SETTINGS] = false;
-            // Implement your "Settings" logic here
+            ++count;
         }
     }
+    return count == 1;
+}
 
-    void menu::draw()
+void menuPause::turnOnButtonKeyDown(std::vector<bool> &buttonsSelected)
+{
+    for (int i = 0; i < m_buttonsSelected.size(); i++)
     {
-        _data->_window->clear();
-        _data->_window->draw(background);
-
-        // condition for loading game, if there is no save file, the load game button will be disabled
-
-        for (int i = 0; i < m_buttons.size(); i++)
+        if (m_buttonsSelected[i] == true)
         {
-            _data->_window->draw(m_buttons[i]);
-        }
-        _data->_window->display();
-    }
-
-    bool menu::isOnlyOneButtonOn(const std::vector<bool> &buttons)
-    {
-        int count = 0;
-        for (int i = 0; i < buttons.size(); i++)
-        {
-            if (buttons[i] == true)
+            m_buttonsSelected[i] = false;
+            if (i == m_buttonsSelected.size() - 1)
             {
-                ++count;
-            }
-        }
-        return count == 1;
-    }
-
-    void menu::turnOnButtonKeyDown(std::vector<bool> & buttonsSelected)
-    {
-        for (int i = 0; i < m_buttonsSelected.size(); i++)
-        {
-            if (m_buttonsSelected[i] == true)
-            {
-                m_buttonsSelected[i] = false;
-                if (i == m_buttonsSelected.size() - 1)
-                {
-                    m_buttonsSelected[0] = true;
-                }
-                else
-                {
-                    m_buttonsSelected[i + 1] = true;
-                }
-                break;
-            }
-        }
-    }
-
-    void menu::turnOnButtonKeyUp(std::vector<bool> & m_buttonsSelected)
-    {
-        for (int i = 0; i < m_buttonsSelected.size(); i++)
-        {
-            if (m_buttonsSelected[i] == true)
-            {
-                m_buttonsSelected[i] = false;
-                if (i == 0)
-                {
-                    m_buttonsSelected[m_buttonsSelected.size() - 1] = true;
-                }
-                else
-                {
-                    m_buttonsSelected[i - 1] = true;
-                }
-                break;
-            }
-        }
-    }
-
-    void menu::turnOnButtonKeyEnter(std::vector<bool> & buttonsSelected, std::vector<bool> & buttonsPressed)
-    {
-        for (int i = 0; i < buttonsSelected.size(); i++)
-        {
-            buttonsPressed[i] = false;
-            if (buttonsSelected[i] == true)
-            {
-                buttonsPressed[i] = true;
-            }
-        }
-    }
-
-    void menu::setColorSelect(std::vector<sf::Text> & m_buttons, std::vector<bool> & m_buttonsSelected)
-    {
-        for (int i = 0; i < m_buttons.size(); i++)
-        {
-            std::cerr << m_buttons.size() << std::endl;
-            if (m_buttonsSelected[i])
-            {
-                m_buttons[i].setFillColor(COLOR_SELECT);
+                m_buttonsSelected[0] = true;
             }
             else
             {
-                m_buttons[i].setFillColor(sf::Color::White);
+                m_buttonsSelected[i + 1] = true;
             }
+            break;
         }
     }
+}
+
+void menuPause::turnOnButtonKeyUp(std::vector<bool> &m_buttonsSelected)
+{
+    for (int i = 0; i < m_buttonsSelected.size(); i++)
+    {
+        if (m_buttonsSelected[i] == true)
+        {
+            m_buttonsSelected[i] = false;
+            if (i == 0)
+            {
+                m_buttonsSelected[m_buttonsSelected.size() - 1] = true;
+            }
+            else
+            {
+                m_buttonsSelected[i - 1] = true;
+            }
+            break;
+        }
+    }
+}
+
+void menuPause::turnOnButtonKeyEnter(std::vector<bool> &buttonsSelected, std::vector<bool> &buttonsPressed)
+{
+    for (int i = 0; i < buttonsSelected.size(); i++)
+    {
+        buttonsPressed[i] = false;
+        if (buttonsSelected[i] == true)
+        {
+            buttonsPressed[i] = true;
+        }
+    }
+}
+
+void menuPause::setColorSelect(std::vector<sf::Text> &m_buttons, std::vector<bool> &m_buttonsSelected)
+{
+    for (int i = 0; i < m_buttons.size(); i++)
+    {
+        std::cerr << m_buttons.size() << std::endl;
+        if (m_buttonsSelected[i])
+        {
+            m_buttons[i].setFillColor(COLOR_SELECT);
+        }
+        else
+        {
+            m_buttons[i].setFillColor(sf::Color::White);
+        }
+    }
+}
