f0a29209662e5f76805d165bc32d1d8896479d57
mvpanhkhoi <lnakhoi21@apcs.fitus.edu.vn>
2023-11-15 21:53:38 +0700

    Update Animal.hpp

--- headers/Animal.hpp
+++ headers/Animal.hpp
@@ -4,20 +4,23 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #pragma once
-class car : public sf::Drawable
+class Animal : public sf::Drawable
 {
     private :
     bool istoright=true;
     float speed;
     data * _data;
-    sf :: Sprite sprite;
+    sf::Sprite sprite;
+    sf::Vector2u size ;
+    sf::Texture texture;
+    int col;
     public :
     void foo()
     {
      
     }
     sf :: FloatRect getGlobalBounds(){return sprite.getGlobalBounds();}
-    void setposcar( sf::Vector2f posblock){ sprite.setPosition( sprite.getPosition().x , posblock.y );}
+    void setposanimal( sf::Vector2f posblock){ sprite.setPosition( sprite.getPosition().x , posblock.y );}
     void turnaround()
     { istoright=!istoright;
     }
@@ -42,10 +45,45 @@ class car : public sf::Drawable
       }
 
     }
-    sf::Vector2f getposcar(){return sprite.getPosition();}
-    car(data* _data) : _data(_data)
+    sf::Vector2f getposanimal(){return sprite.getPosition();}
+    Animal(data* _data) : _data(_data)
     {
-      sprite.setTexture( _data->_assets->getTexture(CAR));
+      int category = rand()%5; //0-Gau,1-Voi,2-Cop,3-Lacda,4-Tegiac
+      switch (category)
+      {
+      case 0: 
+        col = 10;
+        size = _data->_assets->getTexture(GAU).getSize();
+        this->sprite.setTexture(_data->_assets->getTexture(GAU));
+        sprite.setTextureRect(sf::IntRect(0, 0, size.x / col, size.y));
+        break;
+      
+      case 1:
+        col = 12;
+        size = _data->_assets->getTexture(VOI).getSize();
+        this->sprite.setTexture(_data->_assets->getTexture(VOI));
+        sprite.setTextureRect(sf::IntRect(0, 0, size.x / col, size.y));
+        break;
+      case 2:
+        col = 17;
+        size = _data->_assets->getTexture(COP).getSize();
+        this->sprite.setTexture(_data->_assets->getTexture(COP));
+        sprite.setTextureRect(sf::IntRect(0, 0, size.x / col, size.y));
+        break;
+      case 3: 
+        col = 14;
+        size = _data->_assets->getTexture(LACDA).getSize();
+        this->sprite.setTexture(_data->_assets->getTexture(LACDA));
+        sprite.setTextureRect(sf::IntRect(0, 0, size.x / col, size.y));
+        break;
+      case 4:
+        col = 12;
+        size = _data->_assets->getTexture(TEGIAC).getSize();
+        this->sprite.setTexture(_data->_assets->getTexture(TEGIAC));
+        sprite.setTextureRect(sf::IntRect(0, 0, size.x / col, size.y));
+        break;
+      }
+      
      
     int z;
     this->speed=3;
