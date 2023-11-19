2838acee824af30e386dcc2c32452ad96a18cd52
hhplinh <huynhhaphuonglinh@gmail.com>
2023-11-19 21:08:24 +0700

    merge

--- headers/loadSave.hpp
+++ headers/loadSave.hpp
@@ -1,22 +1,22 @@
-#pragma once
-#include <fstream>
+// #pragma once
+// #include <fstream>
 
-class loadSave
-{
-private:
-    std::string path;
-    std::fstream file;
-    std::string line;
-    std::string name;
-    std::string value;
-    std::map<std::string, std::string> data;
+// class loadSave
+// {
+// private:
+//     std::string path;
+//     std::fstream file;
+//     std::string line;
+//     std::string name;
+//     std::string value;
+//     std::map<std::string, std::string> data;
 
-public:
-    loadSave(std::string path);
-    ~loadSave();
-    void load();
-    void save();
-    void set(std::string name, std::string value);
-    std::string get(std::string name);
-    void print();
-};
+// public:
+//     loadSave(std::string path);
+//     ~loadSave();
+//     void load();
+//     void save();
+//     void set(std::string name, std::string value);
+//     std::string get(std::string name);
+//     void print();
+// };
--- src/loadSave.cpp
+++ src/loadSave.cpp
@@ -1,92 +1,92 @@
-/*
-Biến toàn cục: current index //khối cuối màn hình
+// /*
+// Biến toàn cục: current index //khối cuối màn hình
 
--Block size
--> top -> end of stack
-Mỗi stack block có position x, y -> vector 2f
+// -Block size
+// -> top -> end of stack
+// Mỗi stack block có position x, y -> vector 2f
 
--Enemies
-Cụ thể: size + x,y mỗi phần tử
-Car: size
-Animal: size
+// -Enemies
+// Cụ thể: size + x,y mỗi phần tử
+// Car: size
+// Animal: size
 
--player: x,y
-
-
-Load # chơi bth: load cái gần nhất
-Load: class loadMap:  kế thừa map
-player-> init đổi thành player->setPosition
-createMap-> loadMap
-
-ktra file có tồn tại ko, nv chết thì xóa file
-chỉnh isGameSaved
-*/
+// -player: x,y
 
 
-#include "loadSave.hpp"
-#include <iostream>
-#include <vector>
-#include <string>
+// Load # chơi bth: load cái gần nhất
+// Load: class loadMap:  kế thừa map
+// player-> init đổi thành player->setPosition
+// createMap-> loadMap
 
-loadSave::loadSave(std::string path)
-{
-    this->path = path;
-}
+// ktra file có tồn tại ko, nv chết thì xóa file
+// chỉnh isGameSaved
+// */
 
-loadSave::~loadSave()
-{
-}
 
-void loadSave::load()
-{
-    file.open(path, std::ios::in);
-    if (file.is_open())
-    {
-        while (getline(file, line))
-        {
-            name = line.substr(0, line.find('='));
-            value = line.substr(line.find('=') + 1, line.length());
-            data[name] = value;
-        }
-        file.close();
-    }
-    else
-    {
-        std::cerr << "Unable to open file: " << path << std::endl;
-    }
-}
+// #include "loadSave.hpp"
+// #include <iostream>
+// #include <vector>
+// #include <string>
 
-void loadSave::save()
-{
-    file.open(path, std::ios::out);
-    if (file.is_open())
-    {
-        for (auto &i : data)
-        {
-            file << i.first << "=" << i.second << std::endl;
-        }
-        file.close();
-    }
-    else
-    {
-        std::cerr << "Unable to open file: " << path << std::endl;
-    }
-}
+// loadSave::loadSave(std::string path)
+// {
+//     this->path = path;
+// }
 
-void loadSave::set(std::string name, std::string value)
-{
-    data[name] = value;
-}
+// loadSave::~loadSave()
+// {
+// }
 
-std::string loadSave::get(std::string name)
-{
-    return data[name];
-}
+// void loadSave::load()
+// {
+//     file.open(path, std::ios::in);
+//     if (file.is_open())
+//     {
+//         while (getline(file, line))
+//         {
+//             name = line.substr(0, line.find('='));
+//             value = line.substr(line.find('=') + 1, line.length());
+//             data[name] = value;
+//         }
+//         file.close();
+//     }
+//     else
+//     {
+//         std::cerr << "Unable to open file: " << path << std::endl;
+//     }
+// }
 
-void loadSave::print()
-{
-    for (auto &i : data)
-    {
-        std::cerr << i.first << "=" << i.second << std::endl;
-    }
-}
+// void loadSave::save()
+// {
+//     file.open(path, std::ios::out);
+//     if (file.is_open())
+//     {
+//         for (auto &i : data)
+//         {
+//             file << i.first << "=" << i.second << std::endl;
+//         }
+//         file.close();
+//     }
+//     else
+//     {
+//         std::cerr << "Unable to open file: " << path << std::endl;
+//     }
+// }
+
+// void loadSave::set(std::string name, std::string value)
+// {
+//     data[name] = value;
+// }
+
+// std::string loadSave::get(std::string name)
+// {
+//     return data[name];
+// }
+
+// void loadSave::print()
+// {
+//     for (auto &i : data)
+//     {
+//         std::cerr << i.first << "=" << i.second << std::endl;
+//     }
+// }
