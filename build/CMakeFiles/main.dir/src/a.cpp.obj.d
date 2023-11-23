0c0aef9eba3cd8fe1a54a88988ba605713e9f0ac
hhplinh <huynhhaphuonglinh@gmail.com>
2023-11-23 10:16:44 +0700

    make exe available for design modification

---
+++ build/CMakeFiles/main.dir/src/a.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/src/a.cpp.obj: \
+ D:\CrossingRoadGame\CS202_Group-9\src\a.cpp
---
+++ build/CMakeFiles/main.dir/src/loadSave.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/src/loadSave.cpp.obj: \
+ D:\CrossingRoadGame\CS202_Group-9\src\loadSave.cpp
