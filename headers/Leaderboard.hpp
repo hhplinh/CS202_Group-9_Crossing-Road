c558dc83033df0fb24f420f1f99adf2439f95f76
lqhuy04 <128273460+lqhuy04@users.noreply.github.com>
2023-12-04 00:31:04 +0700

    vector of player with highscore

--- headers/Leaderboard.hpp
+++ headers/Leaderboard.hpp
@@ -7,6 +7,37 @@
 #include <string>
 #include <vector>
 
+struct Player {
+    std::vector<Player> players;
+    std::string name;
+    int highScore;
+};
+
+std::vector<Player> loadPlayerData(const std::string& filename) {
+
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return players;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        Player player;
+
+        if (iss >> player.name >> player.highScore) {
+            players.push_back(player);
+        }
+        else {
+            std::cerr << "Error parsing line: " << line << std::endl;
+        }
+    }
+
+    file.close();
+    return players;
+}
+
 class Leaderboard : public state
 {
 private:
