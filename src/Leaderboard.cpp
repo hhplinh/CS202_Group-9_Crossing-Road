4c3e297930e77a4adc7ff98da3e2e3d8ea53d757
hhplinh <huynhhaphuonglinh@gmail.com>
2023-12-11 18:21:32 +0700

    update

--- src/Leaderboard.cpp
+++ src/Leaderboard.cpp
@@ -73,26 +73,6 @@ void Leaderboard::init()
 
             nameText.setString(players[i].name);
             scoreText.setString(std::to_string(players[i].highScore));
-
-            //playerNameText.push_back(nameText);
-           // playerScoreText.push_back(scoreText);
-            /*playerNameText.setString(playerNameText.getString()  + players[i].name + "\n" + "\n");
-            playerScoreText.setString(playerScoreText.getString() + std::to_string(players[i].highScore) + "\n" + "\n");
-            playerNameText.setFont(_data->_assets->getFont(MAIN_FONT));
-            playerNameText.setCharacterSize(55);
-            playerNameText.setFillColor(sf::Color::White);
-            playerNameText.setOutlineColor(_data->_assets->getThemeColor());
-            playerNameText.setOutlineThickness(5.f);
-            playerNameText.setOrigin(playerNameText.getGlobalBounds().width / 2.f, playerNameText.getGlobalBounds().height / 2.f);
-
-            playerScoreText.setFont(_data->_assets->getFont(MAIN_FONT));
-            playerScoreText.setCharacterSize(55);
-            playerScoreText.setFillColor(sf::Color::White);
-            playerScoreText.setOutlineColor(_data->_assets->getThemeColor());
-            playerScoreText.setOutlineThickness(5.f);
-            playerScoreText.setOrigin(playerScoreText.getGlobalBounds().width / 2.f, playerScoreText.getGlobalBounds().height / 2.f);
-            playerNameText.setPosition(_data->_window->getSize().x / 2.f - 65, _data->_window->getSize().y / 2.f + 185);
-            playerScoreText.setPosition(_data->_window->getSize().x / 2.f + 255, _data->_window->getSize().y / 2.f + 192);*/
         }
     }
 }
@@ -129,16 +109,12 @@ void Leaderboard::draw()
     _data->_window->clear();
 
     _data->_window->draw(backgroundSprite);
-    std::vector<Player> players = loadPlayerData("highScore.txt");
- /*   _data->_window->draw(playerNameText);
-    _data->_window->draw(playerScoreText);*/
+    std::vector<Player> players = loadPlayerData(SCORES_PATH);
     for (int i = 0; i < players.size(); i++)
     {
         _data->_window->draw(playerNameText[i]);
         _data->_window->draw(playerScoreText[i]);
     }
-    //     _data->_window->draw(countdownText);
-    // }
     
     _data->_window->display();
 }
