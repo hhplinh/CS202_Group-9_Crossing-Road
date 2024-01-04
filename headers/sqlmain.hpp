5e0bfb1df38b072d02d02de3f3303e109034bfa5
hhplinh <huynhhaphuonglinh@gmail.com>
2024-01-04 22:34:00 +0700

    Optimize

--- headers/sqlmain.hpp
+++ headers/sqlmain.hpp
@@ -1,19 +1,19 @@
-#ifndef SQLMAIN_HPP
-#define SQLMAIN_HPP
+// #ifndef SQLMAIN_HPP
+// #define SQLMAIN_HPP
 
 
-#include <string>
-#include <stdio.h>
-#include <iostream>
-#include <windows.h>
+// #include <string>
+// #include <stdio.h>
+// #include <iostream>
+// #include <windows.h>
 
-#include "sqlite3.h"
+// #include "sqlite3.h"
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName);
-// void run(int argc, char* argv[]);
-// void loadLibSQL();
-void run();
+// static int callback(void *NotUsed, int argc, char **argv, char **azColName);
+// // void run(int argc, char* argv[]);
+// // void loadLibSQL();
+// void run();
 
-void a();
+// void a();
 
-#endif
+// #endif
